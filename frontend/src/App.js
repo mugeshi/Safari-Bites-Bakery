4d2ca811d3dd06f5dab5af069e09083d79d9db97
mugeshi <nicolemugeshi@gmail.com>
2025-02-26 10:37:58 +0300

    updated css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,13 @@
-import React, { useState } from "react";
-import "./components/Navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { 
-  faHome, faUtensils, faShoppingCart, 
-  faClipboardList, faInfoCircle, faPhone, 
-  faUser, faSearch, faBars 
-} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar"; 
 
-function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
+function App() {
   return (
-    <nav className="navbar">
-      {/* Logo */}
-      <h2 className="logo">Bakery</h2>
-
-      {/* Menu Items */}
-      <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-        <li><a href="#"><FontAwesomeIcon icon={faHome} /> Home</a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faUtensils} /> Products</a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faClipboardList} /> Orders</a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faInfoCircle} /> About</a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faPhone} /> Contact</a></li>
-      </ul>
-
-      {/* Right Section: Search, Cart, User */}
-      <div className="right-section">
-        <div className="search-box">
-          <input type="text" placeholder="Search" />
-          <FontAwesomeIcon icon={faSearch} className="icon" />
-        </div>
-        <FontAwesomeIcon icon={faShoppingCart} className="icon" />
-        <FontAwesomeIcon icon={faUser} className="icon" />
-
-        {/* Mobile Menu Button */}
-        <FontAwesomeIcon 
-          icon={faBars} 
-          className="menu-icon" 
-          onClick={() => setMenuOpen(!menuOpen)} 
-        />
-      </div>
-    </nav>
+    <div className="App">
+      <Navbar /> 
+    </div>
   );
 }
 
-export default Navbar;
+export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../images/Logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+  faHome, faUtensils, faShoppingCart, 
+  faClipboardList, faInfoCircle, faPhone, 
+  faUser, faSearch, faBars 
+} from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='container'>
-        <h1>Navbar</h1>
-    </div>
-  )
+    <nav className="navbar">
+      {/* Logo */}
+      <img src={logo} alt="Bakery Logo" className="logo" />
+
+      {/* Menu Items */}
+      <ul className={menuOpen ? "nav-links open" : "nav-links"}>
+        <li><a href="#"><FontAwesomeIcon icon={faHome} /> Home</a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faUtensils} /> Products</a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faClipboardList} /> Orders</a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faInfoCircle} /> About</a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faPhone} /> Contact</a></li>
+      </ul>
+
+      {/* Right Section: Search, Cart, User */}
+      <div className="right-section">
+        <div className="search-box">
+          <input type="text" placeholder="Search" />
+          <FontAwesomeIcon icon={faSearch} className="icon" />
+        </div>
+        <FontAwesomeIcon icon={faShoppingCart} className="icon" />
+        <FontAwesomeIcon icon={faUser} className="icon" />
+
+        {/* Mobile Menu Button */}
+        <FontAwesomeIcon 
+          icon={faBars} 
+          className="menu-icon" 
+          onClick={() => setMenuOpen(!menuOpen)} 
+        />
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
